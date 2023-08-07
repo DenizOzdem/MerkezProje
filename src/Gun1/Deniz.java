fcd962706496fd5ded50aa81d29d63ec23ee1755
DenizOzdem <denizozdem@gmail.com>
2023-08-07 22:04:59 +0300

    Deniz 1

--- src/Gun1/Deniz.java
+++ src/Gun1/Deniz.java
@@ -1,7 +1,8 @@
-package Gun2;
+package Gun1;
 
 public class Deniz {
     public static void main(String[] args) {
-        System.out.println("deniz değişiklik yaptı");
+
+        System.out.println("deniz değiştirdi");
     }
 }
